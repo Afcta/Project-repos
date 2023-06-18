b622e8545508b8e12dfdadec62ba523dacffa529
ChristophHae <christoph.haerendel@gmx.de>
2023-06-18 23:12:36 +0100

    Finished Audio Manager
    finished, may need changes later, but can't do them right now

--- Assets/Script/AudioManager.cs
+++ Assets/Script/AudioManager.cs
@@ -1,19 +1,29 @@
 using UnityEngine.Audio;
 using UnityEngine;
+using System;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
     // Start is called before the first frame update
-    void Start() 
+    void Awake() 
     {
+        foreach(Sound s in sounds){
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.pitch = s.pitch;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update() 
+    public void Play (string name)
     {
-        
+        Sound s .Find(sounds, sound => sound.name = name);
+        if(s == null){
+            Debug.LogWarning("Sound: " + s + " not found !");
+            return;
+        }
+        s.source.Play();
     }
 }
