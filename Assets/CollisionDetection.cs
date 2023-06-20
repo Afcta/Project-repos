c02592b0669d0e46b886653c9237448a70a10895
ChristophHae <christoph.haerendel@gmx.de>
2023-06-20 17:05:32 +0100

    Collision Detection finito
    try it !

--- Assets/CollisionDetection.cs
+++ Assets/CollisionDetection.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject HitParticle;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Enemy")
+        {
+            Debug.Log("Hit !" + other.name);
+            // play enemy anim
+            // other.GetComponent<Animator>().SetTrigger("Hit!");
+            //particle
+            //Instantiate(HitParticle, new Vector3(other.transform.position.x, ...))
+
+
+        }
     }
 }
